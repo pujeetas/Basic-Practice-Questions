463150a831fe078125045a87377c0616c5145a78
pujeetas <pujeeta98@gmail.com>
2024-12-16 21:17:02 +0530

    BasicQues

---
+++ Swap.java
@@ -0,0 +1,13 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a =10;
+        int b =20;
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("Number after swapping " +a + " " +b);
+
+    }
+}
