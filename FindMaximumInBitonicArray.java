0ae265da1f390a896574ade28ab7bbd0465e0dde
pujeetas <pujeeta98@gmail.com>
2024-12-17 14:49:10 +0530

    BitonicArray

---
+++ FindMaximumInBitonicArray.java
@@ -0,0 +1,31 @@
+public class FindMaximumInBitonicArray {
+    public static void main(String[] args) {
+        int[] a = { 10, 8, 6, 4, 2 };
+        int si = 0;
+        int ei = a.length - 1;
+        int result = search(si, ei, a);
+        System.out.println(result);
+
+    }
+
+    public static int search(int si, int ei, int[] a) {
+        if(ei == si){
+            return a[si];
+        }
+        if (ei == si + 1) {
+            if (a[si] >= a[ei]) {
+                return a[si];
+            } else
+                return a[ei];
+        }
+
+        int mid = si + (ei - si) / 2;
+
+        if (a[mid] > a[mid + 1] && a[mid] > a[mid - 1]) {
+            return a[mid];
+        } else if (a[mid] > a[mid - 1] && a[mid] < a[mid + 1]) {
+            return search(mid + 1, ei, a);
+        } else
+            return search(si, mid - 1, a);
+    }
+}
